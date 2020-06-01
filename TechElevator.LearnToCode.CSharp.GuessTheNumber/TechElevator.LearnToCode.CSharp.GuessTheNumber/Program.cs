63e80df4735ada3d8449e0f4b37134b35f3de8bd
Matt Eland <matt.eland@gmail.com>
2020-06-01 16:21:19 -0400

    Integer Variables

--- TechElevator.LearnToCode.CSharp.GuessTheNumber/TechElevator.LearnToCode.CSharp.GuessTheNumber/Program.cs
+++ TechElevator.LearnToCode.CSharp.GuessTheNumber/TechElevator.LearnToCode.CSharp.GuessTheNumber/Program.cs
@@ -9,6 +9,17 @@ namespace TechElevator.LearnToCode.CSharp.GuessTheNumber
             string greetingText = "Hello Variables";
 
             Console.WriteLine(greetingText);
+
+            Console.WriteLine();
+
+            // Integer Variables
+            int num1 = 2;
+            int num2 = 2;
+            int result = num1 + num2;
+
+            Console.WriteLine("First Number: " + num1);
+            Console.WriteLine("Second Number: " + num2);
+            Console.WriteLine("Result: " + result);
         }
     }
 }
