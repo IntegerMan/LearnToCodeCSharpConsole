38c93d6571ec43f4540bc078ffac7775b2c3d66f
Matt Eland <matt.eland@gmail.com>
2020-06-01 16:34:44 -0400

    Removed code from prior examples.

--- TechElevator.LearnToCode.CSharp.GuessTheNumber/TechElevator.LearnToCode.CSharp.GuessTheNumber/Program.cs
+++ TechElevator.LearnToCode.CSharp.GuessTheNumber/TechElevator.LearnToCode.CSharp.GuessTheNumber/Program.cs
@@ -6,18 +6,6 @@ namespace TechElevator.LearnToCode.CSharp.GuessTheNumber
     {
         static void Main(string[] args)
         {
-            string greetingText = "Hello Variables";
-
-            Console.WriteLine(greetingText);
-
-            Console.WriteLine();
-
-            // Integer Variables
-            int num1 = 2;
-            int num2 = 2;
-            int result = num1 + num2;
-            Console.WriteLine(num1 + " + " + num2 + " = " + result);
-
             // Get User Input
             Console.WriteLine("Enter a Number");
 
