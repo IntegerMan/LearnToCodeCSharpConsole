9c5a89a6f8ef717455451f99719a392bdce32264
Matt Eland <matt.eland@gmail.com>
2020-06-01 16:39:43 -0400

    If / else

--- TechElevator.LearnToCode.CSharp.GuessTheNumber/TechElevator.LearnToCode.CSharp.GuessTheNumber/Program.cs
+++ TechElevator.LearnToCode.CSharp.GuessTheNumber/TechElevator.LearnToCode.CSharp.GuessTheNumber/Program.cs
@@ -6,6 +6,8 @@ namespace TechElevator.LearnToCode.CSharp.GuessTheNumber
     {
         static void Main(string[] args)
         {
+            int secretNumber = 42;
+
             // Get User Input
             Console.WriteLine("Enter a Number");
 
@@ -13,6 +15,15 @@ namespace TechElevator.LearnToCode.CSharp.GuessTheNumber
             int userNumber = int.Parse(userNumberText);
 
             Console.WriteLine("You entered: " + userNumber);
+
+            if (userNumber == secretNumber)
+            {
+                Console.WriteLine("That was the number I was thinking of! Amazing!");
+            } 
+            else
+            {
+                Console.WriteLine("Nope. That wasn't my number.");
+            }
         }
     }
 }
